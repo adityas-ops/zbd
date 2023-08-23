86bef68b613ba3cf644f24664d81da5f8cb9cb3a
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-23 11:43:58 +0530

    a

--- components/LandingPage/Hero.jsx
+++ components/LandingPage/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
                     {/* first div */}
                     <div className=' pt-[25px] pb-[30px]'>
                         <h1 className=' sm:text-[64px] text-[2rem] text-center sm:text-start  text-[#f8f8ff] leading-[1.2] sm:leading-[1] font-[900] w-full sm:max-w-[200px] sm:mt-[90px] sm:mb-[20px] '>MONEY WITH MOVES</h1>
-                        <p className='sm:text-[18px] text-[16px] text-center text-[#f8f8ff] leading-[1.5] sm:max-w-[350px] w-full mb-[25px]  sm:mb-[46px] '>Jump into a universe of games and apps, connect with your friends, and earn Bitcoin while having fun.</p>
+                        <p className='sm:text-[18px] text-[16px] sm:text-left text-center text-[#f8f8ff] leading-[1.5] sm:max-w-[350px] w-full mb-[25px]  sm:mb-[46px] '>Jump into a universe of games and apps, connect with your friends, and earn Bitcoin while having fun.</p>
                         <div className='sm:flex hidden '>
                             <Link href="/" className='mr-[20px]'>
                                 <img 
